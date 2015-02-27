b4566a2b8e222417a9a42378fcf9712b7197b3f0
instant-coffee <lachlanrm@gmail.com>
2015-02-27 18:41:03 +0000

    added foundation file

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'sinatra-activerecord'
 
 gem 'puma'
 gem 'tux'
-
+gem 'sqlite3'
 group :development, :test do
   gem 'pry'
   gem 'shotgun'
--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,7 @@
 get '/' do
   erb :index
 end
+
+# get '/authentication' do
+#   redirect "https://www.strava.com/oauth/authorize?client_id=4926&response_type="code"&redirect_uri=http://localhost:3000&scope=read"
+# end
