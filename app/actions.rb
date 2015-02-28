9485d1613fd8ff63df4238683776388428cf7c2b
instant-coffee <lachlanrm@gmail.com>
2015-02-28 03:50:19 +0000

    testing array loops for tables

--- app/actions.rb
+++ app/actions.rb
@@ -3,6 +3,28 @@ get '/' do
   erb :index
 end
 
+# get '/user_checktimes' do
+#   @ary = ['a','b','c','e','f']
+#   erb :'user_checktimes'
+# end
+
+# idea for listing table data
+
+# <table>
+# <% @ary.each do |elem| %>
+#   <tr>
+#     <td>
+#       <%= elem %>
+#     </td>
+#   </tr>
+# <% end %>
+# </table> 
+  
+
+
+
+
+
 # get '/authentication' do
 #   redirect "https://www.strava.com/oauth/authorize?client_id=4926&response_type="code"&redirect_uri=http://localhost:3000&scope=read"
 # end
