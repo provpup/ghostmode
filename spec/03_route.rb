edac3c68fd8a6ff2628f139b61a9c2787dd812ab
Kenny Teng <provpup@users.noreply.github.com>
2015-03-01 20:22:46 +0000

    Add distance calculation (and associated rspec) for route

--- spec/03_route.rb
+++ spec/03_route.rb
@@ -19,14 +19,16 @@ RSpec.describe Route do
       expect(@route.gps_points).to have_exactly(2).items
     end
 
-  end
+    it 'should be able to calculate its total distance' do
+      gps_point1 = GpsPoint.new(latitude: 49.25, longitude: -123.13333)
+      gps_point2 = GpsPoint.new(latitude: 49.5,  longitude: -125.00011)
+      @route.gps_points << gps_point1
+      @route.gps_points << gps_point2
+      @route.save
 
-  describe '.create_checkpoints' do
-
-    it 'should be able to create gps points to be used for checkpoints' do
-
+      expect(@route.distance).to be_within(0.03).of(138.0)
     end
-    
+
   end
   
 end
